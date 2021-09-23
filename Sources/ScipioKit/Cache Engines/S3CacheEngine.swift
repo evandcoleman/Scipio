f219526e76fda910b2b43fa0ef070550a1b3691e
evandcoleman <evandcoleman@users.noreply.github.com>
2021-09-23 14:09:32 -0400

    Add S3 cache engine

---
+++ Sources/ScipioKit/Cache Engines/S3CacheEngine.swift
@@ -0,0 +1,50 @@
+import Combine
+import Foundation
+import PathKit
+
+public struct S3CacheEngine: HTTPCacheEngineProtocol, Decodable, Equatable {
+
+    public let bucket: String
+    public let path: String?
+    public let cdnUrl: URL?
+
+    public var uploadBaseUrl: URL {
+        return bucketS3Url
+    }
+
+    public var downloadBaseUrl: URL {
+        return bucketUrl
+    }
+
+    public let urlSession: URLSession = .createWithExtensionsSupport()
+
+    private var bucketUrl: URL {
+        if let url = cdnUrl {
+            if let path = path {
+                return url
+                    .appendingPathComponent(path)
+            } else {
+                return url
+            }
+        } else {
+            return bucketS3Url
+        }
+    }
+
+    private var bucketS3Url: URL {
+        let baseUrl = URL(string: "https://\(bucket).s3.amazonaws.com")!
+
+        if let path = path {
+            return baseUrl
+                .appendingPathComponent(path)
+        } else {
+            return baseUrl
+        }
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case bucket
+        case path
+        case cdnUrl
+    }
+}
