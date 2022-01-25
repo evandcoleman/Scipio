59ea0fb06042e310f101782904bee06c8af498b0
Evan Coleman <e@edc.me>
2022-01-25 12:18:16 -0500

    update for m1

--- Sources/ScipioKit/Models/Architecture.swift
+++ Sources/ScipioKit/Models/Architecture.swift
@@ -2,18 +2,19 @@ import Foundation
 
 public enum Architecture: String, CaseIterable, CustomStringConvertible {
     case armv7
-    case i386
     case x86_64
     case arm64
 
     public var description: String { rawValue }
 
-    public var sdk: Xcodebuild.SDK {
+    public var possibleSDKs: [Xcodebuild.SDK] {
         switch self {
-        case .armv7, .arm64:
-            return .iphoneos
-        case .x86_64, .i386:
-            return .iphonesimulator
+        case .armv7:
+            return [.iphoneos]
+        case .x86_64:
+            return [.iphonesimulator]
+        case .arm64:
+            return [.iphoneos, .iphonesimulator]
         }
     }
 }
@@ -24,13 +25,14 @@ public extension Xcodebuild.SDK {
         case .iphoneos:
             return [.armv7, .arm64]
         case .iphonesimulator, .macos:
-            return [.x86_64, .i386]
+            return [.x86_64, .arm64]
         }
     }
 }
 
 extension Collection where Iterator.Element == Architecture {
     public var sdkArchitectures: [Xcodebuild.SDK: [Architecture]] {
-        return reduce(into: [:]) { $0[$1.sdk, default: []].append($1) }
+        return Set(flatMap(\.possibleSDKs))
+            .reduce(into: [:]) { $0[$1] = $1.architectures }
     }
 }
